16aaeb856030fb9613dd9969c94bfa4bc2d74c6d
Arnab Sen <arnabsen915@gmail.com>
2025-05-11 15:22:21 +0530

    improvements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Arnab',
+    author_email='arnabsen915@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
